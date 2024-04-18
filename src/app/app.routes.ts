fda3054d95c2fb60ce5c3c4146b0a8bac99980ed
Raffaele Muto <muto@multiprotexion.com>
2024-04-18 17:30:11 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Page1Component } from './pages/page1/page1.component';
 
-export const routes: Routes = [];
+export const  routes: Routes = [
+    { path: '', component: Page1Component },
+   /*  { path: 'login', component: Pg2Component },
+    {path: '404', component: ErrorepgComponent },
+    {path: '**', redirectTo: '404'}, */
+  ];
